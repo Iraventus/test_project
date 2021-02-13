58479fd212685fb8214a0a65214ea4724ebefa6f
Nick Tokarev <tokarev_ns>
2021-02-13 18:17:13 +0300

    Тестовый коммит

--- src/test/java/TestGit.java
+++ src/test/java/TestGit.java
@@ -3,5 +3,6 @@ public class TestGit {
     public void helloWorld() {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
